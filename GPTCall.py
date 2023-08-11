117330e4f1cbc06ddff85cb918767cbd204644d9
La_Nopoly <laurent.nopoly@gbh.fr>
2023-08-11 11:10:58 -0400

    save_to_excel_format_full

--- GPTCall.py
+++ GPTCall.py
@@ -1,6 +1,7 @@
 ﻿import openai
 import pandas as pd
 import json
+import os
 
 
 dash_line = "\n"+"-"*100+"\n"
@@ -18,7 +19,7 @@ api_key = get_keys("C:/Users/La_Nopoly/Desktop/API.json")["DICO_KEY"]
 
 
 # Function to interact with ChatGPT
-def chat_with_gpt3(prompt, max_tokens=400):
+def get_model_response(prompt, max_tokens=500):
     openai.api_key = api_key
     response = openai.Completion.create(
         engine="text-davinci-003",  # You can also try "text-davinci-003" or other engines
@@ -27,7 +28,24 @@ def chat_with_gpt3(prompt, max_tokens=400):
     )
     return response.choices[0].text.strip()
 
- 
+def response_as_dict(model_response):
+    print(model_response)
+    print(dash_line)
+    model_response = model_response[model_response.index('{'):]
+    print(model_response)
+    print(dash_line)
+    return json.loads(model_response)  
+
+def save_to_excel(dictionary,excel_repo = "erp_result.xlsx"):
+    path = f"{excel_repo}"
+    if(os.path.exists(path)):
+        new_df = pd.DataFrame(dictionary)
+        existing_df = pd.read_excel(path)
+        combined_df = pd.concat([existing_df, new_df], ignore_index=True)
+        combined_df.to_excel(path, index=False)
+    else:
+        df = pd.DataFrame(dictionary)
+        df.to_excel(f"{excel_repo}", index=False)
 
 def main(SQL_Query):
     # Fill in your question for ChatGPT here
@@ -41,8 +59,8 @@ def main(SQL_Query):
 
     I count 3 columns so the list length of each key must be 3 
 
-    {{"MAPPING":["Contenant","",""],"COLUMN_NAME_ERP":["TABLE_NAME","COLUMN_NAME","Cube"],"TABLE_NAME_ERP":["COLUMNS","COLUMNS","COLUMNS"],"DATABASE_NAME_ERP":["INFORMATION_SCHEMA","INFORMATION_SCHEMA","INFORMATION_SCHEMA",]}}
-
+    {{"COLUMN_NAME":["","",""],"NOM_EXPLICIT":["","",""],"DATA_TYPE":["","",""],"TABLE_NAME_CUBE":["","",""],"CUBE_NAME":["","",""],"CATALOG_NAME":["","",""],"VIEW_NAME":["","",""],"TABLE_NAME_INFOCENTRE":["","",""],"DATABASE_NAME_INFOCENTRE":["","",""],"SERVEUR_INFOCENTRE":["","",""],"MAPPING":["Contenant","",""],"COLUMN_NAME_ERP":["TABLE_NAME","COLUMN_NAME","Cube"],"TABLE_NAME_ERP":["COLUMNS","COLUMNS","COLUMNS"],"DATABASE_NAME_ERP":["INFORMATION_SCHEMA","INFORMATION_SCHEMA","INFORMATION_SCHEMA",],"ERP_NAME":["","",""],"EXPRESSION":["","",""],"DEFINITION":["","",""],"LIAISON":["","",""]}}
+    
     Example 2:
     SQL Query :    
     select AvionID, Pilote as Employee, Cargo as nbr_passengers, APT as aeroport 
@@ -50,7 +68,7 @@ def main(SQL_Query):
 
     I count 4 columns so the list length of each key must be 4 
 
-    {{"MAPPING":["","Employee","nbr_passengers","aeroport],"COLUMN_NAME_ERP":["AvionID","Pilote","Cargo","APT"],"TABLE_NAME_ERP":["AVION","AVION","AVION","AVION"],"DATABASE_NAME_ERP":["Unknown","Unknown","Unknown","Unknown"]}}
+    {{"COLUMN_NAME":["","","",""],"NOM_EXPLICIT":["","","",""],"DATA_TYPE":["","","",""],"TABLE_NAME_CUBE":["","","",""],"CUBE_NAME":["","","",""],"CATALOG_NAME":["","","",""],"VIEW_NAME":["","","",""],"TABLE_NAME_INFOCENTRE":["","","",""],"DATABASE_NAME_INFOCENTRE":["","","",""],"SERVEUR_INFOCENTRE":["","","",""],"MAPPING":["","Employee","nbr_passengers","aeroport],"COLUMN_NAME_ERP":["AvionID","Pilote","Cargo","APT"],"TABLE_NAME_ERP":["AVION","AVION","AVION","AVION"],"DATABASE_NAME_ERP":["Unknown","Unknown","Unknown","Unknown"],"ERP_NAME":["","","",""],"EXPRESSION":["","","",""],"DEFINITION":["","","",""],"LIAISON":["","","",""]}}
 
     Complete this one
     SQL Query:
@@ -62,22 +80,9 @@ def main(SQL_Query):
  
 
     # Get response from ChatGPT
-    model_response = chat_with_gpt3(prompt)
+    data = response_as_dict(get_model_response(prompt))
 
-    print(model_response)
-    print(dash_line)
-
-    model_response = model_response[model_response.index('{'):]
-
-    print(model_response)
-
-
-
-    model_tab_response = json.loads(model_response)  
-
-    # Save ChatGPT's response to an Excel file
-    df = pd.DataFrame(model_tab_response)
-    df.to_excel('chatgpt_response.xlsx', index=False)
+    save_to_excel(data)
 
  
 
