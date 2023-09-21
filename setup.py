b9af832591f861644e7337b5c77094c42ff3f531
La_Nopoly <laurent.nopoly@gbh.fr>
2023-09-21 18:39:26 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
-DESCRIPTION = 'A package to build a data dictionary from xml MS SQL SERVER file'
-with open("README.md","r") as f:
+VERSION = '0.5'
+DESCRIPTION = 'A package to build a data dictionary from xml MS SQL SERVER file and select SQL query'
+with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
     f.close()
 
-#Setting up
+# Setting up
 setup(
     name="datadict_toolbox",
-    version = VERSION,
-    author = "LauNop (Laurent NOPOLY)",
-    author_email = "laurentnopoly@gmail.com",
-    description = DESCRIPTION,
+    version=VERSION,
+    author="Laurent NOPOLY",
+    author_email="laurentnopoly@gmail.com",
+    description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="",
-    packages = find_packages(),
-    install_requires = ["openai","pandas","openpyxl",],
-    keywords = ['python','data','dictionary','governance'],
+    url="https://github.com/LauNop/datadict_toolbox",
+    packages=find_packages(),
+    install_requires=["openai", "pandas", "openpyxl", ],
+    keywords=['python', 'data', 'dictionary', 'governance', 'SQL'],
     license="",
-    classifiers = [
-        "Development Status :: Developping ERP solution ",
-        "Intended Audience :: Deveoppers",
+    classifiers=[
+        "Development Status :: Realease package ",
+        "Intended Audience :: Developpers",
         "Programming Language :: Python :: 3",
         "Operating System :: Microsoft :: Windows"
     ]
